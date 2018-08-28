54a5212794b6554af569450e4bd46c16476c07e8
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-28 10:47:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,3 @@
 curl -X GET \
--H 'Authorization: Bearer {8XiBgy0V7wVEXpkmckGTfTHO5LcdQYk5iO/Skvn/E3n7RdRtfrK5mk07kDa0EfKUuR9vuwrY8kPvOBM82VlTd3daGTzC6HH6/6i9D+6kAIP0TUqIjiIX6GkczHmUzit0wGWQGUW2boC/6oBKuHymcAdB04t89/1O/w1cDnyilFU=}' \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
 https://api.line.me/v1/oauth/verify
