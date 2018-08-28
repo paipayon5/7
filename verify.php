a4385190f83959fb82402069a23cf9f87a68149d
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-28 11:01:40 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = '8XiBgy0V7wVEXpkmckGTfTHO5LcdQYk5iO/Skvn/E3n7RdRtfrK5mk07kDa0EfKUuR9vuwrY8kPvOBM82VlTd3daGTzC6HH6/6i9D+6kAIP0TUqIjiIX6GkczHmUzit0wGWQGUW2boC/6oBKuHymcAdB04t89/1O/w1cDnyilFU=';
 
+
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
