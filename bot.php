a23dc31f8a04ed86aab50933392900b10f93b32b
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-28 11:55:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,10 +16,11 @@ if (!is_null($events['events'])) {
    // Get replyToken
    $replyToken = $event['replyToken'];
    $replyToken2 = $event['replyToken'];
+  $text2 =$event['replyToken']['text'];
    // Build message to reply back
    $messages = [
     'type' => 'text',
-    'text' => '$replyToken2'
+    'text' => '$text2'
    ];
    // Make a POST Request to Messaging API to reply to sender
    $url = 'https://api.line.me/v2/bot/message/reply';
