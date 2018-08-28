5f939cd074939bd928a98fce71ff4cad2f2852f5
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-28 10:50:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,38 @@
-curl -X GET \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
-https://api.line.me/v1/oauth/verify
+<?php
+ require("pub.php");
+ require("line.php");
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['ESP'])) {
+	
+	send_LINE($events['ESP']);
+		
+	echo "OK";
+	}
+if (!is_null($events['events'])) {
+	echo "line bot";
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+
+			$Topic = "NodeMCU1" ;
+			getMqttfromlineMsg($Topic,$text);
+			   
+			
+		}
+	}
+}
+echo "OK3";
+?>
