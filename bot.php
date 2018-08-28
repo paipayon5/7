8ae05809b0fbc7a2f3500b84a59be3c08cdea532
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-28 11:23:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
    // Build message to reply back
    $messages = [
     'type' => 'text',
-    '123' => $text
+    'text' => $text
    ];
 
    // Make a POST Request to Messaging API to reply to sender
