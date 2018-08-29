b013d7486e28a9b84111f42a8916a02a0eebefd5
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-29 11:46:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@
    //รับ id ของผู้ใช้
    $id = $arrayJson['events'][0]['source']['userId'];
 $i = array("Ue3b41e181bccb599dd963e7bf301ddd8", "Ue3b41e181bccb599dd963e7bf301ddd8", "Ue3b41e181bccb599dd963e7bf301ddd8");
-if($message == "A"){  
+//if($message == "A"){  
 for($i as $value){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
@@ -18,7 +18,7 @@ for($i as $value){
       $arrayPostData['messages'][1]['type'] = "text";
       $arrayPostData['messages'][1]['text'] = "$i";
       pushMsg($arrayHeader,$arrayPostData);
-   }
+   //}
 }
    function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
