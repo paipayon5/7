22414b92d319c8a7e37ea2e3fc65b87059d3ff03
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-08-29 15:44:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,6 +31,7 @@ $objDB = mssql_select_db("PAItest");
 $strSQL = "INSERT INTO USERLINE ";
 $strSQL .="(userline) ";
 $strSQL .="$i2";
+echo $i2;
 }
    function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
