a9cbfd9c3fb04ff4d1f2c441d53eab38d2227516
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-09-03 17:19:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@
    //รับ id ของผู้ใช้
    $id = $arrayJson['events'][0]['source']['userId'];
    #ตัวอย่าง Message Type "Text + Sticker"
-   if($id <> ''){
+   if($id <> ""){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
       $arrayPostData['messages'][0]['text'] = "$id";
