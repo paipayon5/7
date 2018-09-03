63921dc03f73bf2e9319db509064f176cc03e22e
paipayon5 <42636536+paipayon5@users.noreply.github.com>
2018-09-03 10:26:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,13 +9,13 @@
    $message = $arrayJson['events'][0]['message']['text'];
    //รับ id ของผู้ใช้
    $id = $arrayJson['events'][0]['source']['userId'];
-if($id <> ''){ $arrayPostData['to'] = $id;
+   $id2 ="Ue3b41e181bccb599dd963e7bf301ddd8";
+if($id <> ''){ $arrayPostData['to'] = $id2;
       $arrayPostData['messages'][0]['type'] = "text";
       $arrayPostData['messages'][0]['text'] = "$id";
       pushMsg($arrayHeader,$arrayPostData);
              }
 $i = array("Ue3b41e181bccb599dd963e7bf301ddd8", "Ue3b41e181bccb599dd963e7bf301ddd8", "Ue3b41e181bccb599dd963e7bf301ddd8");
-$i2 ="Ue3b41e181bccb599dd963e7bf301ddd8";
 if($message == "A"){  
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
